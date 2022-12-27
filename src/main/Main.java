01c994e39bb29cc87028762b6dd569971b5c2a82
Ehedei Hernandez <ehernandez@eturia.es>
2022-12-27 14:03:47 +0000

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * @author {@link "mailto:ehernandez@eturia.es"}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
