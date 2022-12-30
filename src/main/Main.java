c91bd62017bb703933070fe413a7acfef07e38e0
Ehedei Hernandez <ehernandez@eturia.es>
2022-12-30 14:12:26 +0000

    Topic 3 Exercise

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,18 @@
 package main;
 
 /**
- * @author {@link "mailto:ehernandez@eturia.es"}
+ * @author {@link "mailto:ehedeidev@gmail.com"}
  */
 public class Main {
+    private static final double GENERAL_IVA = 21;
+    public static double getPriceWithIva(final double price) {
+        final double percentage = price / 100 * GENERAL_IVA;
+        return price + percentage;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final double price = 125.50;
+
+        System.out.println("Precio del producto: " + price);
+        System.out.println("Precio con IVA: " + getPriceWithIva(price));
     }
 }
